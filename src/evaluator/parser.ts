755a99da1888fc14b464c2bdb483727fff0b30f2
fe-martinez <femartinez@fi.uba.ar>
2024-05-17 14:48:07 -0300

    agrego funcion para recolectar pares de un ruleset

--- src/evaluator/parser.ts
+++ src/evaluator/parser.ts
@@ -1,8 +1,46 @@
 import fs from 'fs';
-import { RuleSet } from './types';
+import { Condition, Rule, RuleSet, DataCondition, CallCondition } from './types';
+import { ConditionType } from './conditionTypeEnum';
 
 export function parseRules(filePath: string ): RuleSet {
     const rulesData = fs.readFileSync(filePath, 'utf8');
     const ruleSet: RuleSet = JSON.parse(rulesData);
     return ruleSet;
+}
+
+export function collectPairsFromRuleSet(ruleSet: RuleSet): string[] {
+    const pairs: string[] = [];
+    ruleSet.rules.forEach(rule => {
+        const rulePairs = extractExchangePairs(rule);
+        pairs.push(...rulePairs)
+    })
+    return pairs
+}
+
+export function extractExchangePairs(rule: Rule): string[] {
+    const pairs: string[] = [];
+    traverse(rule.condition, pairs);
+    return pairs;
+}
+
+function traverse(condition: Condition, pairs: string[]) {
+    if (condition.type === ConditionType.CALL) {
+        handleCallCondition(condition, pairs);
+    }
+}
+
+function handleCallCondition(condition: CallCondition, pairs: string[]) {
+    condition.arguments.forEach(arg => {
+        if (arg.type === ConditionType.DATA) {
+            handleDataCondition(arg as DataCondition, pairs);
+        } else {
+            traverse(arg, pairs);
+        }
+    });
+}
+
+function handleDataCondition(condition: DataCondition, pairs: string[]) {
+    if ('symbol' in condition) {
+        pairs.push(condition.symbol);
+    }
 }

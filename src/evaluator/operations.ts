e88dc5cd5eb0ef9844bababa86e6c1e0c558789d
claram97 <cruano@fi.uba.ar>
2024-05-17 18:00:55 -0300

    agrego throw de error si se recibe lista vacía en mayor

--- src/evaluator/operations.ts
+++ src/evaluator/operations.ts
@@ -10,7 +10,9 @@ const sum = (args: Value[]): number =>
   (args as number[]).reduce((acc, val) => acc + val, 0)
 
 const greaterThan = (args: Value[]): boolean =>
-  args.every((value, i, arr) => i === 0 || (value as number) > (arr[i - 1] as number))
+  args.length > 0
+  ? args.every((value, i, arr) => i === 0 || (value as number) > (arr[i - 1] as number))
+  : (() => { throw new Error('Invalid number of arguments') })();
 
 const lessThan = (args: Value[]): boolean =>
   args.length > 0

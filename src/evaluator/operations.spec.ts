2010fb6a44f2574b5f9d2d40bb6eb278de674ff7
claram97 <cruano@fi.uba.ar>
2024-05-17 18:00:18 -0300

    agrego validación de throw error con lista vacía en la función de mayor

--- src/evaluator/operations.spec.ts
+++ src/evaluator/operations.spec.ts
@@ -29,6 +29,7 @@ describe('getOperation', () => {
         expect(operation([1, 2, 1])).toBe(false);
         expect(operation([1, 1, 1])).toBe(false);
         expect(operation([1])).toBe(true);
+        expect(() => operation([])).toThrow();
     })
     it('should perform the less than (<) operation correctly', () => {      
         const operation = getOperation('<');

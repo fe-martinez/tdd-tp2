9eab3e07f424f387b479f4ef8853511ed927c649
claram97 <cruano@fi.uba.ar>
2024-05-17 18:47:23 -0300

    agrego test a not

--- src/evaluator/operations.spec.ts
+++ src/evaluator/operations.spec.ts
@@ -119,6 +119,7 @@ describe('getOperation', () => {
     it('should perform the not operation correctly', () => {
         const operation = getOperation('NOT');
         expect(operation([true])).toBe(false);
+        expect(operation([false])).toBe(true);
     })
     it('should throw an error for undefined operation', () => {
         const operation = getOperation('undefined');

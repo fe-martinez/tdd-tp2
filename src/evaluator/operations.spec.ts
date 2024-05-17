66a1b272d17c242183c80afbe5bb1a3fbd663334
claram97 <cruano@fi.uba.ar>
2024-05-17 17:55:03 -0300

    agrego test para lista vacía en menor igual

--- src/evaluator/operations.spec.ts
+++ src/evaluator/operations.spec.ts
@@ -53,6 +53,7 @@ describe('getOperation', () => {
         expect(operation([1, 2, 1])).toBe(false);
         expect(operation([1, 1, 1])).toBe(true);
         expect(operation([1])).toBe(true);
+        expect(() => operation([])).toThrow();
     })
     it('should perform the negate operation correctly', () => {
         const operation = getOperation('NEGATE');

e9c6c2b6d45867b46402d0e81b032d6a4fc6bb8d
claram97 <cruano@fi.uba.ar>
2024-05-18 00:00:51 -0300

    agrego test de true or true

--- src/evaluator/operations.spec.ts
+++ src/evaluator/operations.spec.ts
@@ -141,6 +141,7 @@ describe('getOperation', () => {
         expect(operation([false, false, false])).toBe(false);
         expect(operation([false])).toBe(false);
         expect(operation([true, false])).toBe(true);
+        expect(operation([true, true])).toBe(true);
     })
     it('should throw an error for undefined operation', () => {
         const operation = getOperation('undefined');

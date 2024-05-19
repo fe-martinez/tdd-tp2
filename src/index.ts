58b593cacbe70cedec5e7fc262696364df27aad0
Paula Bruck <85425148+paulabruck@users.noreply.github.com>
2024-05-18 21:13:57 -0300

    wallet connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { collectPairsFromRuleSet, parseRules } from './evaluator/parser';
 import { connectToBinanceWebSocket, getUri } from './evaluator/binanceConnection';
-
+import { placeOrder } from './binanceApi';
 //Esto no sé por qué no funciona, es lo que debería permitirnos obtener las reglas del archivo rules.json
 // function getPairsFromFile(filePath: string): any {
 //   let ruleSet = parseRules('/home/claram97/tdd/tdd-tp2/src/evaluator/rules.json');
@@ -17,9 +17,21 @@ function getPairsFromFile(filePath: string): any {
     return { pairs, ruleSet };
   }
 let { pairs, ruleSet } = getPairsFromFile('/Users/paulabruck/Desktop/FIUBA/Tecnicas_De_Diseño/tdd-tp2/src/evaluator/rules.json');
-
+console.log('Pairs from file:', pairs);
 
 //let pairs = ['BTC/USDT', 'ETH/USDT', 'ADA/USDT'];
 
 let URI = getUri(pairs);
 connectToBinanceWebSocket(URI, ruleSet);
+
+(async () => {
+    try {
+      const buyOrder = await placeOrder('BTCUSDT', 'BUY', 0.01);
+      console.log('Buy Order:', buyOrder);
+  
+      const sellOrder = await placeOrder('BTCUSDT', 'SELL', 0.01);
+      console.log('Sell Order:', sellOrder);
+    } catch (error) {
+      console.error('Error in placing orders:', error);
+    }
+  })();

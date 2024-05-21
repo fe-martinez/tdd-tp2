5af62b6f9c4eaa1378e0b4da15030d38ef96c4c8
claram97 <cruano@fi.uba.ar>
2024-05-20 22:31:46 -0300

    no hice nada en realidad xd solo dejé código comentado en index

--- src/index.ts
+++ src/index.ts
@@ -27,9 +27,9 @@ import { placeOrder , getOrderHistory} from './binanceApi';
 
 (async () => {
     try {
-      console.log('Placing buy order...');
-      const buyOrder = await placeOrder('BTCUSDT', 'BUY', 0.01);
-      console.log('Buy Order:', buyOrder);
+      // console.log('Placing buy order...');
+      // const buyOrder = await placeOrder('BTCUSDT', 'BUY', 0.01);
+      // console.log('Buy Order:', buyOrder);
   
       console.log('Placing sell order...');
       const sellOrder = await placeOrder('BTCUSDT', 'SELL', 0.01);

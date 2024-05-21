00b6c651e14e189026ead2aaf1f61245a9d39287
claram97 <cruano@fi.uba.ar>
2024-05-20 23:47:58 -0300

    actualizo binanceConnection

--- src/data/binanceConnection.ts
+++ src/data/binanceConnection.ts
@@ -1,55 +1,56 @@
 import WebSocket from 'ws';
 import { getEndpointURI, getOrderBook, parsePairs } from './binanceParser';
-import { RuleSet } from '../model/types'; 
-import { evaluateRules } from '../evaluator/rulesEvaluator'; 
+import { addHistoricalData, getLastPairValue } from './database';
+import { EventEmitter } from 'events';
 
-function openConnection(ws: WebSocket) {
-  ws.on('open', () => {
-    console.log('Conexión WebSocket abierta con Binance');
-  }); 
+const IMPORTANT_VARIATION = 0.001;
+
+export class BinanceListener extends EventEmitter {
+  private binanceWs: WebSocket;
+  private textDecoder: TextDecoder;
+
+  constructor(pairs: string[]) {
+    super();
+    this.binanceWs = (connectToBinanceWebSocket(getUri(pairs)));
+    this.setupListener();
+    this.textDecoder = new TextDecoder();
+  }
+
+  private setupListener() {
+    this.binanceWs.on('message', (data: Buffer) => {
+      const arrayBuffer = Uint8Array.from(data).buffer;
+      const jsonString = this.textDecoder.decode(arrayBuffer);
+
+      const orderBook = getOrderBook(jsonString);
+      const lastSavedValue = getLastPairValue(orderBook.symbol);
+      if (isImportantChange(orderBook, lastSavedValue)) {
+        addHistoricalData(orderBook.symbol, { bestBidPrice: orderBook.bestBidPrice, bestAskPrice: orderBook.bestAskPrice, time: new Date() });
+        this.emit('update', { symbol: orderBook.symbol, bestBidPrice: orderBook.bestBidPrice, bestAskPrice: orderBook.bestAskPrice, time: new Date() });
+      }
+    });
+
+    this.binanceWs.on('error', (error) => {
+      console.error('Error en la conexión WebSocket:', error);
+    });
+
+    this.binanceWs.on('open', () => {
+      console.log('Conexión WebSocket abierta con Binance');
+    }); 
+
+  }
 }
 
-function errorConnection(ws: WebSocket) {
-  ws.on('error', (error) => {
-    console.error('Error en la conexión WebSocket:', error);
-  });
-}
-
-function handleMessage(ws: WebSocket, ruleSet: RuleSet) {
-  ws.on('message', (data: Buffer) => {
-    const textDecoder = new TextDecoder();
-    const arrayBuffer = Uint8Array.from(data).buffer;
-    const jsonString = textDecoder.decode(arrayBuffer);
-
-    try {
-      let orderBook = getOrderBook(jsonString);
-      console.log(`Nuevo evento para el símbolo ${orderBook.symbol} con ID de actualización ${orderBook.updateID}, mejor precio de oferta ${orderBook.bestBidPrice} y cantidad ${orderBook.bestBidQuantity}, mejor precio de venta ${orderBook.bestAskPrice} y cantidad ${orderBook.bestAskQuantity}`);
-      
-      evaluateRules(ruleSet);
-    } catch (error) {
-      console.error('Error al analizar los datos JSON:', error);
-    }
-  });
-}
-
-
-// {
-  //   "u":400900217,     // order book updateId
-  //   "s":"BNBUSDT",     // symbol
-  //   "b":"25.35190000", // best bid price
-  //   "B":"31.21000000", // best bid qty
-  //   "a":"25.36520000", // best ask price
-  //   "A":"40.66000000"  // best ask qty
-  // }
-export function connectToBinanceWebSocket(URI: string, ruleSet: RuleSet) {
+export function connectToBinanceWebSocket(URI: string): WebSocket {
     const ws = new WebSocket(URI);
-  
-    openConnection(ws);
-    handleMessage(ws, ruleSet);
-    errorConnection(ws);
+    return ws;
 }
 
 export function getUri(pairs: string[]): string {
     const parsedPairs = parsePairs(pairs);
     return getEndpointURI(parsedPairs);
+}
+
+function isImportantChange(orderBook: any, lastValue: number): boolean {
+  const bands = [lastValue * (1 - IMPORTANT_VARIATION), lastValue * (1 + IMPORTANT_VARIATION)];
+  return parseFloat(orderBook.bestBidPrice) < bands[0] || parseFloat(orderBook.bestBidPrice) > bands[1];
 }

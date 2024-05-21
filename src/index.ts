56df14772dcd1619683a13ed899d398a44ae03d9
claram97 <cruano@fi.uba.ar>
2024-05-20 23:46:57 -0300

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { collectPairsFromRuleSet, parseRules } from './evaluator/parser';
-import { connectToBinanceWebSocket, getUri } from './data/binanceConnection';
-import { placeOrder , getOrderHistory} from './data/binanceApi';
-import { sendMessage } from './notifier/notificationSender';
+import { RuleSet } from './model/types';
+//import { BinanceListener } from './data/binanceConnection';
+import { evaluateRules } from './evaluator/rulesEvaluator';
+import { ConditionEvaluator } from './dynamic-evaluator/conditionEvaluator';
+import { executeRuleSet } from './dynamic-evaluator/rulesEvaluator';
 
 // creo q asi deberia funcionar
 // function getPairsFromFile(filePath: string): any {
@@ -47,3 +49,26 @@ import { sendMessage } from './notifier/notificationSender';
 //     }
 // })();
 //sendMessage("Hola bebetos, tengo logo, estoy liste para informar las alertas de Binance, soy un bot NO BINARIE")
+
+let ruleSet: RuleSet = parseRules('src/rules.json');
+let pairs = collectPairsFromRuleSet(ruleSet);
+
+const compiledRules = new ConditionEvaluator(ruleSet);
+const binanceData: BinanceListener = new BinanceListener(pairs);
+
+binanceData.on('error', (error) => {
+  console.error('WebSocket error:', error);
+});
+
+binanceData.on('connected', () => {
+  console.log('WebSocket connected');
+});
+
+binanceData.on('disconnected', ({ code, reason }) => {
+  console.log(`WebSocket disconnected (${code}): ${reason}`);
+});
+
+binanceData.on('update', (data) => {
+  console.log('Update from binance:', data);
+  evaluateRules(ruleSet);
+});

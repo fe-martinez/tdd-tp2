bd0f0eafb2ef580417ec39901d49d22143bd2262
Ramiro Gestoso <rgestoso@fi.uba.ar>
2024-05-23 20:13:45 -0300

    call condition es recursivo

--- src/model/callConditionEvaluator.spec.ts
+++ src/model/callConditionEvaluator.spec.ts
@@ -1,6 +1,7 @@
 // import ConstantConditionEvaluator from './constantConditionEvaluator';
 import CallConditionEvaluator from "./callConditionEvaluator";
 import ConstantConditionEvaluator from "./constantConditionEvaluator";
+import VariableConditionEvaluator from "./variableConditionEvaluator";
 
 describe('ConstatConditionEvaluator', () => {
     const variables = new Map<string, number>();
@@ -8,5 +9,24 @@ describe('ConstatConditionEvaluator', () => {
         const args = [new ConstantConditionEvaluator(5000), new ConstantConditionEvaluator(5000)];
         const constantValueConditionEvaluator = new CallConditionEvaluator('==', args);
         expect(constantValueConditionEvaluator.evaluate(variables)).toBe(true);
+    });
+
+    it('should evaluate variable arguments and execute operation', () => {
+        const variableName = "MY_VARIABLE_NAME";
+        variables.set(variableName, 10000);
+        const args = [new ConstantConditionEvaluator(5000), new VariableConditionEvaluator(variableName)];
+        const constantValueConditionEvaluator = new CallConditionEvaluator('>', args);
+        expect(constantValueConditionEvaluator.evaluate(variables)).toBe(false);
+    });
+
+    it('should evaluate recursive call arguments and execute operation', () => {
+        const variableName = "MY_VARIABLE_NAME";
+        variables.set(variableName, 10000);
+        const args = [new ConstantConditionEvaluator(5000), new VariableConditionEvaluator(variableName)];
+        const constantValueConditionEvaluator = new CallConditionEvaluator('>', args);
+        
+        const args2 = [constantValueConditionEvaluator, new ConstantConditionEvaluator(false)];
+        const constantValueConditionEvaluator2 = new CallConditionEvaluator('AND', args2);
+        expect(constantValueConditionEvaluator2.evaluate(variables)).toBe(false);
     });
 });

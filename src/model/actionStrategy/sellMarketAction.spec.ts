4b1dbf1d3cb73eccff8a2252aff2f60767de334d
Ramiro Gestoso <rgestoso@fi.uba.ar>
2024-05-25 12:19:22 -0300

    agrego sell market action con tests

---
+++ src/model/actionStrategy/sellMarketAction.spec.ts
@@ -0,0 +1,28 @@
+import { ConditionEvaluatorType } from "../conditionStrategy/conditionEvaluator";
+import ConstantConditionEvaluator from "../conditionStrategy/constantConditionEvaluator";
+import { InvalidAmountError } from "../MarketDataApi/marketDataApi";
+import TestMarketDataApi from "../MarketDataApi/testMarketDataApi";
+import { SellMarketAction } from "./sellMarketAction";
+
+describe('BuyMarketAction', () => {
+    const marketDataApi = new TestMarketDataApi();
+    const variables = new Map<string, ConditionEvaluatorType>();
+    beforeEach(() => {
+        marketDataApi._setWallet('BTC', 10);
+        marketDataApi._setWallet('USDT', 1000);
+        marketDataApi._setWallet('ETH', 20);
+    });
+    it('sellMarket decreases base wallet amount', async () => {
+        const condition = new ConstantConditionEvaluator(1);
+        const action = new SellMarketAction(marketDataApi, 'BTC/USDT', condition);
+        await action.execute(variables);
+        const btcWallet = await marketDataApi.getWallet("BTC");
+        expect(btcWallet).toBe(9);
+    });
+
+    it('sellMarket throws error if condition does not resolve to number', async () => {
+        const condition = new ConstantConditionEvaluator("1");
+        const action = new SellMarketAction(marketDataApi, 'BTC/USDT', condition);
+        await expect(action.execute(variables)).rejects.toThrow(InvalidAmountError);
+    });
+});

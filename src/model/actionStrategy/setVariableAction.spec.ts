2a0dd01c0659a4f97d474330bd933062bae122b4
Ramiro Gestoso <rgestoso@fi.uba.ar>
2024-05-25 12:03:28 -0300

    agrego action strategy con set variable

---
+++ src/model/actionStrategy/setVariableAction.spec.ts
@@ -0,0 +1,25 @@
+import { SetVariableAction } from "./setVariableAction";
+
+describe('SetVariableAction', () => {
+    const variables = new Map<string, number>();
+    const variableName = 'testVariable';
+    variables.set(variableName, 10);
+
+    it('setVariableAction sets existent variable', async () => {
+        const setVariableAction = new SetVariableAction(variableName, 20);
+
+        setVariableAction.execute(variables);
+        const value = variables.get(variableName);
+        expect(value).toBe(20);
+    });
+
+    it('setVariableAction sets new variable by creating it', async () => {
+        const newVariableName = 'newVariable';
+        const setVariableAction = new SetVariableAction(newVariableName, "20");
+
+        setVariableAction.execute(variables);
+        const value = variables.get(newVariableName);
+        expect(value).toBe("20");
+    });
+
+});
---
+++ src/model/actionStrategy/setVariableAction.ts
@@ -0,0 +1,16 @@
+import { ConditionEvaluatorType, ConditionEvaluatorVariables } from "../conditionStrategy/conditionEvaluator";
+import { Action } from "./action";
+
+export class SetVariableAction implements Action {
+    private variableName: string;
+    private value: ConditionEvaluatorType;
+
+    constructor(variableName: string, value: ConditionEvaluatorType) {
+        this.variableName = variableName;
+        this.value = value;
+    }
+    execute(variables: ConditionEvaluatorVariables): Promise<void> {
+        variables.set(this.variableName, this.value);
+        return Promise.resolve();
+    }
+}

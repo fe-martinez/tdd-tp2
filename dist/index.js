4780c84a3db5fe98da176b3c75c5add36539fe21
Paula Bruck <85425148+paulabruck@users.noreply.github.com>
2024-06-03 14:02:26 -0300

    Errores mas especificos para place order

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ const binanceConnection_1 = require("./data/binanceConnection");
 const notificationSender_1 = require("./notifier/notificationSender");
 const rulesEvaluator_1 = __importDefault(require("./model/ruleEvaluator/rulesEvaluator"));
 const fs_1 = require("fs");
+const binanceApi_1 = require("./data/binanceApi");
 let ruleSet = (0, parser_1.parseRules)('src/rules.json');
 let pairs = (0, parser_1.collectPairsFromRuleSet)(ruleSet);
 const notifier = new notificationSender_1.MessageNotifier();
@@ -33,13 +34,13 @@ binanceData.on('update', () => {
     console.log('WebSocket update');
 });
 //Para probar que la wallet siga funcionando
-// async function order() {
-//   try {
-//     var order = await placeOrder('BTCUSDT', 'BUY', 0.001);
-//     console.log(order);
-//   }
-//   catch (error) {
-//     console.error('Error al enviar la orden:', error);
-//   }
-// }
-// order();
+async function order() {
+    try {
+        var order = await (0, binanceApi_1.placeOrder)('BTCUSDT', 'BUY', 0.001);
+        console.log(order);
+    }
+    catch (error) {
+        //console.error('Error al enviar la orden:', error);
+    }
+}
+order();

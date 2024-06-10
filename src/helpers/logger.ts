dbe8ee607db5bb4d1f63a494fcc3253a6597bb88
Ramiro Gestoso <rgestoso@fi.uba.ar>
2024-06-10 19:47:47 -0300

    feature: logger helper permite enviar las notificaciones

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,9 +1,24 @@
 import fs from 'fs'
-const filename = process.env.NODE_ENV === 'development' ? 'development.log' : 'production.log'
+import { MessageNotifier } from "../notifier/notificationSender";
 
-export default function(message: string, type: string = "INFO") {
+const isTest = process.env.NODE_ENV === 'test';
+const filename = isTest ? 'test.log' : 'production.log'
+
+export function sendNotification(message: string) {
+    const notifier = MessageNotifier.getInstance();
+    notifier.sendNotification(message);
+}
+
+export function logToFile(message: string, type: string = "INFO") {
     const logType = type.toUpperCase();
     const date = new Date().toISOString()
     const log = `${date} - ${logType}: ${message}\n`
     fs.appendFileSync(filename, log);
+}
+
+export function logAndSendNotification(message: string, type: string = "INFO") {
+    if (!isTest) {
+        sendNotification(message);
+    }
+    logToFile(message, type);
 }

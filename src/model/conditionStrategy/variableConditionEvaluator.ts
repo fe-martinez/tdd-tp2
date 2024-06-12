dd614a6c92c5c9fe8e55c362b7142c4aca15b78b
Ramiro Gestoso <rgestoso@fi.uba.ar>
2024-06-12 19:22:08 -0300

    fix bug cuando variable es falsey

--- src/model/conditionStrategy/variableConditionEvaluator.ts
+++ src/model/conditionStrategy/variableConditionEvaluator.ts
@@ -17,7 +17,7 @@ export default class VariableConditionEvaluator implements ConditionEvaluator {
 
     evaluate(variables: ConditionEvaluatorVariables): Promise<ConditionEvaluatorType> {
         const value = variables.get(this.variableName);
-        if (!value) {
+        if (value === undefined) {
             throw new InexistentVariableError(`Variable ${this.variableName} not found`);
         }
         return Promise.resolve(value);
